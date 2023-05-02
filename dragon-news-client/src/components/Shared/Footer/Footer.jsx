320fe05cd25ffe0cdeb3706deb6446cc856ca173
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-02 21:15:51 +0600

    Created right nav
    created the right navbar

--- dragon-news-client/src/components/Shared/Footer/Footer.jsx
+++ dragon-news-client/src/components/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <p className='text-center p-4 bg-dark text-light'><small>copyright of my news</small></p>
+        <p className='text-center p-4 bg-dark text-light mb-0'><small>copyright of my news</small></p>
     );
 };
 
--- dragon-news-client/src/components/Shared/RightNav/RightNav.jsx
+++ dragon-news-client/src/components/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle , FaGithub , FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
     return (
         <div>
-            
+            <h4>Login With</h4>
+                <Button variant="outline-danger" className='mb-2'><FaGoogle></FaGoogle> Login With Google</Button>
+                <Button variant="outline-secondary"><FaGithub></FaGithub> Login With Google</Button>
+                <div>
+                    <h4>Find us on</h4>
+                    <ListGroup>
+                        <ListGroup.Item variant='primary'><FaFacebook></FaFacebook> facebook</ListGroup.Item>
+                        <ListGroup.Item variant='warning'><FaInstagram></FaInstagram> instagram</ListGroup.Item>
+                        <ListGroup.Item variant='primary'><FaTwitter></FaTwitter> twitter</ListGroup.Item>
+                    </ListGroup>
+                    <QZone></QZone>
+                    <div>
+                        <img className='img-fluid' src={bg} alt="" />
+                    </div>
+                </div>
         </div>
     );
 };
